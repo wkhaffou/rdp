ffd5ed985f8c959a13c32183de980f5cd999c354
wkhaffou <83318937+wkhaffou@users.noreply.github.com>
2021-05-06 12:01:35 +0000

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 printf "Installing RDP to your system:" >&2
 {
-sudo useradd -m LPTECH
-sudo adduser LPTECH sudo
-echo 'LPTECH:8426' | sudo chpasswd
+sudo useradd -m WKRDP2021
+sudo adduser WKRDP2021 sudo
+echo 'WKRDP2021:8426' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -18,13 +18,13 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
-sudo adduser LPTECH chrome-remote-desktop
+sudo adduser WKRDP2021 chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - LPTECH -c """$CRP"""
+su - WKRDP2021 -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
